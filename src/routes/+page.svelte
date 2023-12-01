2caffc619aa75fd97261d6f316bb80c651833eec
Ian Chen <115192648+ionpng@users.noreply.github.com>
2023-12-01 22:58:21 +0000

    added linking

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -206,10 +206,22 @@
 								{#if message.board == currentBoard} 
 									<div class="w-[70vw]"> 
 										{#if timeFirst}
+										{#if message.content.includes("https://")}
+										<a href={message.content} target="_blank"> 
+											{timeConverter(message.sent_at)} - {message.sender}: <span class="text-blue-500 hover:text-blue-700 no-underline">{message.content}</span>
+										</a>
+									{:else}
 										{timeConverter(message.sent_at)} - {message.sender}: {message.content}
-										{:else}
+									{/if}
+									{:else}
+									{#if message.content.includes("https://")}
+										<a href={message.content} target="_blank"> 
+											{message.sender}: <span class="text-blue-500 hover:text-blue-700 no-underline">{message.content}</span> @ {timeConverter(message.sent_at)}
+										</a>
+									{:else}
 										{message.sender}: {message.content} @ {timeConverter(message.sent_at)}
-										{/if}
+									{/if}
+									{/if}
 									</div>
 								{/if}
 							{/each}
