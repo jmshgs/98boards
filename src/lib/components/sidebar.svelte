1d7aac5ccffe92be197cb1ea2c08d97526660386
Ian Chen <115192648+ionpng@users.noreply.github.com>
2024-03-05 19:47:27 +0000

    revert button css

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -40,15 +40,16 @@
             </ul>
         </div>
         <div class="flex items-end">
-            <button class="justify-center flex w-1/2 p-2 m-2 {themeColor=='light' ? "bg-slate-200": ""} {newButtonClass}" on:click={() => {
+            <button class="justify-center flex w-full p-2 m-2 {themeColor=='light' ? "bg-slate-200": ""} {newButtonClass}" on:click={() => {
                 oldUI = !oldUI;
             }}>
                 {oldUI ? "new ui" : "old ui"}
             </button>
-            <button class="justify-center flex w-1/2 p-2 m-2 {themeColor=='light' ? "bg-slate-200": ""}  {newButtonClass}" on:click={() => {showSettings=true;}}>
+        </div>
+        <div class="flex items-end">
+            <button class="justify-center flex w-full p-2 m-2 {themeColor=='light' ? "bg-slate-200": ""}  {newButtonClass}" on:click={() => {showSettings=true;}}>
                 settings
             </button>
-
         </div>
         <div class="flex items-end">
             <button class="justify-center flex w-full p-2 m-2 {themeColor=='light' ? "bg-slate-200": ""} {newButtonClass}" on:click={() => {
@@ -56,11 +57,14 @@
             }}>
                 create board
             </button>
+        </div>
+        <div class="flex items-end">
             <button class="justify-center flex w-full p-2 m-2 {themeColor=='light' ? "bg-slate-200": ""} {newButtonClass}" on:click={() => {
                 joinBoard = true;
             }}>
                 join a board
             </button>
+
         </div>
         <div class="flex items-end">
             <button class="justify-center flex w-full p-2 m-2 {themeColor=='light' ? "bg-slate-200": ""} {newButtonClass}" on:click={() => {
